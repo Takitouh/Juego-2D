4b5eac6415b34d9ccb0c47a246c3ddf8538e3b50
Takitouh <1097097476jhon@gmail.com>
2025-06-22 16:48:30 -0500

    refactor: fixed tileManager for new tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -12,12 +12,12 @@ import java.util.Objects;
 
 public class TileManager {
     GamePanel gamePanel;
-    Tile[] tiles;
-    int[][] mapTileNum;
+    public Tile[] tiles;
+    public int[][] mapTileNum;
 
     public TileManager(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
-        tiles = new Tile[10];
+        tiles = new Tile[11];
         mapTileNum = new int[gamePanel.maxworldCol][gamePanel.maxworldRow];
         this.getTilesImage();
         this.loadMap("maps/world01.txt");
@@ -31,9 +31,11 @@ public class TileManager {
 
               tiles[1] = new Tile();
               tiles[1].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/wall.png")));
+              tiles[1].collision = true;
 
               tiles[2] = new Tile();
               tiles[2].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/water.png")));
+              tiles[2].collision = true;
 
               tiles[3] = new Tile();
               tiles[3].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/dirt.png")));
@@ -43,9 +45,24 @@ public class TileManager {
 
               tiles[5] = new Tile();
               tiles[5].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/basictree.png")));
+              tiles[5].collision = true;
 
               tiles[6] = new Tile();
               tiles[6].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/appletree.png")));
+              tiles[6].collision = true;
+
+              tiles[7] = new Tile();
+              tiles[7].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/dandelion.png")));
+
+              tiles[8] = new Tile();
+              tiles[8].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/daisy.png")));
+
+              tiles[9] = new Tile();
+              tiles[9].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/woodenplate.png")));
+
+              tiles[10] = new Tile();
+              tiles[10].img = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/lemontree.png")));
+              tiles[10].collision = true;
 
           }catch (IOException e){
               e.printStackTrace();
@@ -58,8 +75,8 @@ public class TileManager {
          while(worldCol < gamePanel.maxworldCol && worldRow < gamePanel.maxworldRow){
              int tileNum = mapTileNum[worldCol][worldRow];
 
-             int worldX = worldCol * gamePanel.TileSize;
-             int worldY = worldRow * gamePanel.TileSize;
+             int worldX = worldCol * gamePanel.tileSize;
+             int worldY = worldRow * gamePanel.tileSize;
              int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
              int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
 
