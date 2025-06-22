a7dd255145e892a563fbcd33d374dd45c72c3650
Takitouh <1097097476jhon@gmail.com>
2025-06-21 19:09:23 -0500

    feature: attribute check collision for entities

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,7 +1,11 @@
 package entity;
 
+import java.awt.*;
+
 public class Entity {
     public int worldX, worldY;
     public int speed;
     public int spriteNum = 1, spriteCount = 0;
+    public Rectangle hitBox;
+    public boolean collisionOn;
 }
