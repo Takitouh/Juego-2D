ec63fee91961c3c1f94fb1580f7bda611f8ce631
Takitouh <1097097476jhon@gmail.com>
2025-07-02 19:30:56 -0500

    feature: implemented setupGame method of gamepanel

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-
+        gamePanel.setupGame();
         gamePanel.starGameThread();
 
     }
