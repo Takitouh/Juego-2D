11f9dd7bddb2e3e7f71fea97626587eec96f66e9
Takitouh <1097097476jhon@gmail.com>
2025-07-03 09:08:03 -0500

    Added attributes direction and hitBoxDefaultX, hitBoxDefaultY

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -7,5 +7,7 @@ public class Entity {
     public int speed;
     public int spriteNum = 1, spriteCount = 0;
     public Rectangle hitBox;
+    public String direction;
+    public int hitBoxDefaultX, hitBoxDefaultY;
     public boolean collisionOn;
 }
